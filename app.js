c27a8139faedc134cea3de90080f0ef7a6a270f7
JaveriyaSaleem <javeriyasaleem678@gmail.com>
2024-10-20 20:18:46 +0500

    added

---
+++ app.js
@@ -0,0 +1,7 @@
+document.getElementById('submit').addEventListener("click",function(){
+    // document.getElementById('right-section') = 
+})
+document.getElementsByName('option-bg').addEventListener("click",function(){
+    console.log("hi")
+
+})
