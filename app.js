66e14d402892f3a3408b095433015d1fd832bdec
JaveriyaSaleem <javeriyasaleem678@gmail.com>
2024-10-20 21:05:10 +0500

    added

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 document.getElementById('submit').addEventListener("click",function(){
     // document.getElementById('right-section') = 
 })
-document.getElementsByName('option-bg').addEventListener("click",function(){
-    console.log("hi")
-
+let bgVar = document.getElementsByName('option-bg')
+bgVar.forEach((value)=>{
+    console.log(value.value)
+    value.addEventListener("click",function(){
+        
+      let classVar = document.getElementById('right-section').classList[3]
+      document.getElementById('right-section').classList.replace(classVar,value.value)
+    }
+)
 })
